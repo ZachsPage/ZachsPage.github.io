900a61b2b7884bddbfe4a9a41f6f919d5d293b72
Zach Miller <xhmf1423@gmail.com>
2025-12-01 22:44:40 -0500

    Convert tabs to spaces (#6)

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,12 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	type: 'content',
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		pubDate: z.coerce.date(),
-	}),
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        pubDate: z.coerce.date(),
+    }),
 });
 
 export const collections = { blog };
